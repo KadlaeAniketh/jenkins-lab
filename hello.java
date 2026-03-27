768c47d229a54f49b5b0181db2914a22ba9b39d0
KADLAE ANIKETH <145157725+KadlaeAniketh@users.noreply.github.com>
2026-03-27 22:38:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ import java.net.*;
 class hello{
 public static void main(String args[]){
     System.out.println("hello world");
-    System.out.println("HOw are you";
+    System.out.println("HOw are 
 }
 }
